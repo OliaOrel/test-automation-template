3b6c7c56db0835a6bbe543bcdec9357d4713bc90
Olia Orel <olia.orel13@gmail.com>
2021-11-26 17:33:49 +0200

    Implemented Header Banner Test

---
+++ AutomationTemplate/UI.Tests/GeneralTests/HeaderBannerTest.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using OpenQA.Selenium;
+using NUnit.Framework;
+using System;
+
+namespace UI.Tests.GeneralTests
+{
+    [TestFixture]
+    class HeaderBannerTest : BaseGeneralTest
+    {
+        [Test]
+        public void HeaderBanner_ClickOnBanner_SaleProducts()
+        {
+            //arrange
+           
+            //act
+            var MyMainPage = Header.ClickHeaderBanner();
+            var ProductContainers = MyMainPage.ProductContainers;
+            var DiscountPercentage = MyMainPage.DiscountPercentage;
+            int ProductContainersCounter = WebElementHelper.ElementCounter(ProductContainers);
+            int DiscountPercentageCounter = WebElementHelper.ElementCounter(DiscountPercentage);
+
+            Console.WriteLine(ProductContainersCounter);
+            Console.WriteLine(DiscountPercentageCounter);
+
+            //assert
+            ProductContainersCounter.Should().Be(DiscountPercentageCounter);
+        }
+    }
+}
