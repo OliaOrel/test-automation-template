d96c79e65b677edc27da31c866c5fe6388f9038a
Olia Orel <olia.orel13@gmail.com>
2021-11-30 15:26:25 +0200

    Implemented Logo test

---
+++ AutomationTemplate/UI.Tests/GeneralTests/LogoTest.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using OpenQA.Selenium;
+
+namespace UI.Tests.GeneralTests
+{
+    [TestFixture]
+    public class LogoTest : BaseGeneralTest
+    {
+        [Test]
+        public void Logo_Click_LoadMainPage()
+        {
+            //arrange
+
+            //act
+            Header.ClickLogo();
+            string currentURL = WebElementHelper.GetUrl();
+
+            Console.WriteLine(currentURL);
+
+            //assert
+            currentURL.Should()
+                      .Be("http://automationpractice.com/index.php");
+        }
+    }
+}
