f116ef1869ee5a9ba894613e09613d9e4b6d67f6
Olia Orel <olia.orel13@gmail.com>
2021-12-08 14:30:39 +0200

    Implemented categories tests

---
+++ AutomationTemplate/PageObjects/CategoryPage.cs
@@ -0,0 +1,14 @@
+﻿using OpenQA.Selenium;
+using UI.PageObjects.Base;
+
+namespace UI.PageObjects
+{
+    public class CategoryPage : BaseAutomationPracticePage
+    {
+        public CategoryPage(IWebDriver driver) : base(driver)
+        {
+        }
+
+        public By CategoryTitleBy() => By.ClassName("cat-name");
+    }
+}
--- AutomationTemplate/PageObjects/Shared/Header.cs
+++ AutomationTemplate/PageObjects/Shared/Header.cs
@@ -11,6 +11,14 @@ namespace UI.PageObjects.Shared
 
         public By HeaderBanner => By.XPath("//div[@class='banner']");
 
+        public By Logo => By.ClassName("logo");
+
+        public By WomenButton => By.XPath("//a[@title='Women']");
+
+        public By DressesButton => By.XPath("//div/ul/li/a[@title='Dresses']");
+
+        public By TshirtsButton => By.XPath("//div/ul/li/a[@title='T-shirts']");
+
         public Header(IWebDriver driver) : base(driver)
         {
         }
@@ -45,5 +53,38 @@ namespace UI.PageObjects.Shared
 
             return new MainPage(Driver);
         }
+
+        public MainPage ClickLogo()
+        {
+            FindElement(Logo)
+                .Click();
+
+            return new MainPage(Driver);
+        }
+
+        public CategoryPage ClickWomenButton()
+        {
+            FindElement(WomenButton)
+                .Click();
+
+            return new CategoryPage(Driver);
+        }
+
+        public CategoryPage ClickDressesButton()
+        {
+            FindElement(DressesButton)
+                .Click();
+
+            return new CategoryPage(Driver);
+        }
+
+        public CategoryPage ClickTshirtsButton()
+        {
+            FindElement(TshirtsButton)
+                .Click();
+
+            return new CategoryPage(Driver);
+        }
+
     }
 }
